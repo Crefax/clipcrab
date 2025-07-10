481bc7fdb2ca29de9987b00134ced562a0deb361
Crefax <crefaxx@gmail.com>
2025-07-10 04:25:46 +0300

    feat: add system tray minimize to tray
    - Add system tray icon with right-click menu (Show/Hide/Quit)
    - Prevent app close, minimize to tray instead
    - Left-click tray icon to show/focus window
    - Right-click for context menu options

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,11 +3,94 @@ pub mod database;
 pub mod clipboard;
 pub mod commands;
 
+use tauri::{
+    Manager, WindowEvent,
+    image::Image,
+    menu::{MenuBuilder, MenuItem},
+    tray::TrayIconBuilder,
+};
+
+fn enable_tray(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    // Create menu items
+    let show_item = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
+    let hide_item = MenuItem::with_id(app, "hide", "Hide", true, None::<&str>)?;
+    let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+    // Build menu
+    let menu = MenuBuilder::new(app)
+        .item(&show_item)
+        .separator()
+        .item(&hide_item)
+        .separator()
+        .item(&quit_item)
+        .build()?;
+
+    // Create tray icon
+    let _tray = TrayIconBuilder::with_id("tray")
+        .icon(Image::from_bytes(include_bytes!("../icons/icon.png"))?)
+        .menu(&menu)
+        .tooltip("ClipCrab - Clipboard Manager")
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "show" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.show();
+                    let _ = window.set_focus();
+                }
+            }
+            "hide" => {
+                if let Some(window) = app.get_webview_window("main") {
+                    let _ = window.hide();
+                }
+            }
+            "quit" => {
+                app.exit(0);
+            }
+            _ => {}
+        })
+        .on_tray_icon_event(|tray, event| {
+            match event {
+                tauri::tray::TrayIconEvent::Click { 
+                    button: tauri::tray::MouseButton::Left, 
+                    .. 
+                } => {
+                    if let Some(app) = tray.app_handle().get_webview_window("main") {
+                        if app.is_visible().unwrap_or(false) {
+                            let _ = app.set_focus(); // Gizleme yerine sadece focus yap
+                        } else {
+                            let _ = app.show();
+                            let _ = app.set_focus();
+                        }
+                    }
+                }
+                tauri::tray::TrayIconEvent::Click { 
+                    button: tauri::tray::MouseButton::Right, 
+                    .. 
+                } => {
+                    // Sağ tıklamada menü otomatik olarak gösterilir, hiçbir şey yapmamız gerekmez
+                }
+                _ => {}
+            }
+        })
+        .build(app)?;
+
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .on_window_event(|window, event| match event {
+            WindowEvent::CloseRequested { api, .. } => {
+                let _ = window.hide();
+                api.prevent_close();
+            }
+            _ => {}
+        })
         .setup(|app| {
+            if let Err(e) = enable_tray(app) {
+                eprintln!("Tray icon setup failed: {}", e);
+            }
             clipboard::start_clipboard_watcher(app.handle().clone());
             Ok(())
         })
